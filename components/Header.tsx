615d8ccf4741690f26814fef52f5da13beaa46f2
AnshumanRShar <59237628+AnshumanRShar@users.noreply.github.com>
2024-08-28 12:18:41 -0700

    dark mode changes + motto

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,18 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 // config
 import config from "@/config/general";
-import logokropp1 from "@/public/logokropp1.png"; // Importing the image
+import logokropp2 from "@/public/logokropp2.png"; // Importing the image
 
 const Header = () => {
   return (
     <header
       className="flex-col sm:flex-row flex justify-between items-center"
-      style={{ backgroundColor: "#0d1b2a", marginLeft: "3rem" }}
+      style={{
+        backgroundColor: "#ffffff",
+        marginLeft: "0rem",
+        paddingLeft: "1rem",
+      }}
     >
       <div className="flex items-center">
         <Link href="/">
           <Image
-            src={logokropp1} // Using the imported image
+            src={logokropp2} // Using the imported image
             alt="Kropp Logo"
             width={40} // Adjust width as needed
             height={60} // Adjust height as needed
